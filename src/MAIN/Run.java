6c8cdf3d30cf320720115bbb0d28b2243b056661
zomtet <zomet.alon@gmail.com>
2018-12-19 14:19:57 +0200

    aids added

--- src/MAIN/Run.java
+++ src/MAIN/Run.java
@@ -11,13 +11,12 @@ public class Run {
     public static void main(String[] args) {
         Model.initiateScreen();
         Model.initiateMenu();
+        System.out.println("aids");
     }
-    public static void printArr(int[][] a)
-    {
-        for(int i = 0; i < Const.col; i++)
-        {
-            for(int j = 0; j < Const.row; j++)
-            {
+
+    public static void printArr(int[][] a) {
+        for (int i = 0; i < Const.col; i++) {
+            for (int j = 0; j < Const.row; j++) {
                 System.out.print(a[i][j] + ",");
             }
             System.out.println();
